f994b064c9354b00207c561c485a8d4376cada70
Cameron Wiese <camwiese@gmail.com>
2020-03-17 16:09:05 -0700

    Update names.rb

--- names.rb
+++ names.rb
@@ -1,4 +1,4 @@
-unction toTitleCase(str) {
+Function toTitleCase(str) {
   str = str.toLowerCase().split(' ');
   for (var i = 0; i < str.length; i++) {
     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
